cd4d43057f73ff63e0d3f257688278315fe59aef
tash-9 <3222.tasfiaislam@gmail.com>
2026-08-01 19:37:21 +0600

    updated API integration with Postman testing

--- report.js
+++ report.js
@@ -1,24 +1,18 @@
 const newman = require('newman');
-require('dotenv').config();
+//require('dotenv').config();
  
 newman.run({
    
-    collection:require('./Dmoney_API_Integration_Testing.postman_collection.json'),
+    collection:require('./dMoney_API_Integration_Testing.postman_collection.json'),
     reporters: 'htmlextra',
     iterationCount: 1,
-     delayRequest: 5000, 
-         envVar: [
-        {        
-            key: "gmailToken",
-            value: process.env.token
-        }
-    ],
+     delayRequest: 5000,
     reporter: {
         htmlextra: {
             export: './Reports/report.html',
         }
     }
-}, function (err) {
-    if (err) { throw err; }
-    console.log('collection run complete!');
-});
+    }, function (err) {
+        if (err) { throw err; }
+        console.log('collection run complete!');
+    });
